bb6df356cc1b5be830b247a99f8579e06ea4afb8
dakotachatt <dvchatt@gmail.com>
2023-11-01 06:59:59 -0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Dakota Chatt on 2023-11-01.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
